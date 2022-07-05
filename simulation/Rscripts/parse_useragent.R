bb0a962317d6aee9e5aa1607695b553707c471ce
kurokida <kurokid@kyudai.jp>
2022-07-05 14:23:06 +0900

    Add a program to parse the user agent information.

---
+++ simulation/Rscripts/parse_useragent.R
@@ -0,0 +1,14 @@
+# Library for parsing userAgent
+library(uaparserjs)
+# Read data
+ds = read.table("sub_dat.csv", sep = ",", header = T)
+# Parse userAgent
+userAgents = ua_parse(ds$userAgent)
+# Info about all devices
+userAgents[,c("device.family", "device.brand", "device.model")]
+# Info about the non-Windows and Mac OS X devices 
+userAgents[
+  !(userAgents$os.family %in% c("Windows", "Mac OS X")),
+  c("os.family", "os.major", "device.family", "device.brand", "device.model")
+]
+write.csv(userAgents, "user_agent.csv")
